aae22d3e44c19d3c4a4b17d35cc6358293e01fb3
Kaurin <milos.kaurin@gmail.com>
2018-12-16 04:53:37 +0000

    Proxy launcher changes: * Now exiting on any error (trapped) * Created a cleanup function to be invoked via trap on 1/SIGINT/SIGTERM signals * Changed the way I get the AMI: Proxy launcher changes: * Now exiting on any error (trapped) * Created a cleanup function to be invoked via trap on 0/1/SIGINT signals * Changed the way I get the AMI: https://kaurin.github.io/2018/Practicing-JQ-and-JMESPath/ * Added a control var for SSH timeout (5 by default) * Sleeping for 1 second instead of 5 for SSH retries

--- proxy_launcher.sh
+++ proxy_launcher.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
+set -e
 
 SSH_KEYPATH="~/.ssh/milos-london-aws.pem"
 SG_ID="sg-a044f2c9"
 AWS_SSH_KEYNAME="milos-london-aws"
 export AWS_DEFAULT_REGION="eu-west-2"
+SSH_TIMEOUT=5
+
+
+cleanup() {
+  echo "Cleanup: Starting cleanup"
+  if  [ "$AWS_INSTANCEID" == "" ] ; then
+    echo "Cleanup: nothing to do. Exiting."
+    exit 0
+  fi
+  echo "Cleanup: Terminating the instance"
+  aws ec2 terminate-instances --instance-ids "$AWS_INSTANCEID"
+  exit 0
+}
+trap cleanup 1 SIGINT SIGTERM
 
 AWS_MINIMAL_AMI=$(
-  aws --query 'Images[*].[Name,ImageId]' \
-      --output text \
-      ec2 describe-images \
-        --owners amazon \
-        --filters \
-          "Name=root-device-type,Values=ebs" \
-          "Name=architecture,Values=x86_64" \
-          "Name=virtualization-type,Values=hvm" \
-          "Name=image-type,Values=machine" \
-          "Name=is-public,Values=true" | grep minimal | sort | tail -n1 | awk '{print $2}'
+  aws ssm get-parameter \
+    --name /aws/service/ami-amazon-linux-latest/amzn-ami-minimal-hvm-x86_64-ebs \
+    --query 'Parameter.Value' \
+    --output text
 )
 
 echo "Latest Amazon Linux AMI: $AWS_MINIMAL_AMI ..."
@@ -45,12 +54,13 @@ echo "Public IP: $AWS_PUBLICIP"
 retries=0
 repeat=true
 
+# Retry forever until a succesful SSH connection is established
 while "$repeat"; do
   ((retries+=1)) &&
   echo "Try number $retries..." &&
-  ssh -oStrictHostKeyChecking=no -D 50000 -i "$SSH_KEYPATH" ec2-user@"$AWS_PUBLICIP" && repeat=false
+  ssh -o ConnectTimeout="$SSH_TIMEOUT" -o StrictHostKeyChecking=no -D 50000 -i "$SSH_KEYPATH" ec2-user@"$AWS_PUBLICIP" && repeat=false
   if "$repeat"; then
-    sleep 5
+    sleep 1
   fi
 done
 
